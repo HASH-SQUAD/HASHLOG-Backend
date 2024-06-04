79defe602ec0fb8a0111be762dae1b61eaf17d92
Jamkris <dltmdgus1412@gmail.com>
2024-06-04 23:12:20 +0900

    ADD :: 사용자 정보 조회시 어드민도 조회가능하도록함

--- routes/User/LoginState.js
+++ routes/User/LoginState.js
@@ -5,6 +5,7 @@ const LoginState = async (req, res) => {
 		authUtil.successTrue(201, '회원상태 조회완료', {
 			nickname: req.user.dataValues.nickname,
 			email: req.user.dataValues.email,
+			isAdmin: req.user.dataValues.isAdmin,
 		})
 	);
 };
