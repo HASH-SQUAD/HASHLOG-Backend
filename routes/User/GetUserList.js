0eac82bac792a141885e7ea7008965da83686515
Jamkris <dltmdgus1412@gmail.com>
2024-06-18 16:23:47 +0900

    ADD :: 유저 조회 완성

--- routes/User/GetUserList.js
+++ routes/User/GetUserList.js
@@ -3,9 +3,22 @@ const authUtil = require('../../response/authUtil');
 const { Users } = require('../../models');
 
 const GetUserList = async (req, res) => {
+	console.log(req.user.dataValues.isAdmin);
+
+	const pageNum = req.param('page');
+	let offset = 0;
+	if (pageNum > 1) {
+		offset = 20 * (pageNum - 1);
+	}
+
+	if (!req.user.dataValues.isAdmin) {
+		return res.status(200).send(authUtil.successFalse(200, '어드민만 조회가능합니다.'));
+	}
 	try {
-		await Users.findAll().then(data => {
-			console.log(data);
+		await Users.findAll({
+			offset: offset,
+			limit: 20,
+		}).then(data => {
 			if (data) {
 				res.status(200).send(authUtil.successTrue(200, '유저를 찾았습니다.', data));
 			}
@@ -16,4 +29,4 @@ const GetUserList = async (req, res) => {
 	}
 };
 
-module.exports = GetUserList
+module.exports = GetUserList;
